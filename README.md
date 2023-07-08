50861f4e677acff40c13d5e89852876f3ea6499e
Danylo Sus <danil.sushko19@gmail.com>
2023-07-08 11:44:37 +0300

    Initial

---
+++ README.md
@@ -0,0 +1,7 @@
+# Interactive rating component
+
+From [Frontend Mentor](www.frontendmentor.io)
+
+- [ ] Desktop design
+- [ ] Responsive design
+- [ ] Animations
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Net from "./Elements/Net";
+
+function App() {
+  return(
+    <>
+      <Net need=" " />
+      <h1>Sus</h1>
+    </>
+  )
+}
+
+export default App;
